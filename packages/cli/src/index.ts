1d4f60cd755e7addccd2647472f4fc71da0f968a
Stephan Boersma <boersma@live.dk>
2021-03-23 11:51:07 +0100

    .

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -9,21 +9,19 @@ const packageJson = require('../package.json');
 
 program
   .version(packageJson.version)
-  .description('Prototype CLI for thesis')
+  .description('React Bratus CLI')
   .option('-s, --start', 'Start server')
   .option('-c, --compile', 'Compile prototype project')
   .parse(process.argv);
 
 const options = program.opts();
 if (options.start) {
-  console.log(program.opts());
-  console.log(process.cwd());
   if (fs.existsSync(`${process.cwd()}/graphData.json`)) {
-    const s = new Server();
-    s.listen();
+    const server = new Server();
+    server.listen();
   } else {
     const parser = new ASTParser(`${process.cwd()}/src`);
-    console.log(parser);
+    parser.compile();
     const server = new Server();
     server.listen();
   }
@@ -31,5 +29,5 @@ if (options.start) {
 
 if (options.compile) {
   const parser = new ASTParser(`${process.cwd()}/src`);
-  console.log(parser);
+  parser.compile();
 }
