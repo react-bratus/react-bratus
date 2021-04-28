c4aa182007a84387c24d81e3318d9519c90e49c1
Stephan Boersma <boersma@live.dk>
2021-04-28 12:10:57 +0200

    Added changelog and highlighting to help component

--- packages/app/src/styles/molecules/Help/index.js
+++ packages/app/src/styles/molecules/Help/index.js
@@ -1,16 +1,17 @@
 import { Divider, Drawer, Typography } from 'antd';
 import Checkbox from 'antd/lib/checkbox/Checkbox';
 import PropTypes from 'prop-types';
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ReactFlow, { useZoomPanHelper } from 'react-flow-renderer';
 import styled from 'styled-components';
 
+import HighlightedComponentsContext from '../../../contexts/HighlightedComponentsContext';
 import { getLayoutedElements } from '../../../utils/graphUtils';
 import ComponentNode from '../../atoms/ComponentNode';
 
 const Wrapper = styled.div`
   width: 600px;
-  height: 500px;
+  height: 700px;
 `;
 const { Title, Paragraph, Link } = Typography;
 
@@ -21,6 +22,29 @@ const Help = ({
   setHideHelpOnStartUp,
 }) => {
   const { setCenter } = useZoomPanHelper();
+  const [ellipsis] = useState(true);
+  const { highlightedComponent, setHighlightedComponent } = useContext(
+    HighlightedComponentsContext
+  );
+  const highlightComponent = (node, lock) => {
+    const componentName = node ? node.data.label : null;
+    if (lock) {
+      setHighlightedComponent({
+        componentName: componentName,
+        locked: lock,
+      });
+    } else if (!highlightedComponent.locked) {
+      setHighlightedComponent({
+        componentName: componentName,
+        locked: highlightedComponent.locked,
+      });
+    }
+  };
+  const resetHighlight = () => {
+    if (!highlightedComponent.locked) {
+      setHighlightedComponent({ componentName: null, locked: false });
+    }
+  };
 
   useEffect(() => setCenter(350, 350, 0.7));
   return (
@@ -41,6 +65,13 @@ const Help = ({
         You can always click the Help button in navigation pane on the right to
         see this window again.
       </Paragraph>
+      <Paragraph>
+        I hope that you have time to give me feedback. Any feature ideas,
+        constructive criticism, or bugs are welcomed in the{' '}
+        <Link href="https://github.com/stephanboersma/react-bratus/issues/new?assignees=&labels=feedback&template=feedback.md&title=%5BFeedback%5D">
+          GitHub repository.
+        </Link>
+      </Paragraph>
       <Checkbox
         checked={hideHelpOnStartUp}
         onChange={(e) => setHideHelpOnStartUp(e.target.checked)}
@@ -49,22 +80,34 @@ const Help = ({
       </Checkbox>
 
       <Divider />
-      <Title level={3}>Feedback</Title>
-      <Paragraph>
-        I hope that you have time to give me feedback. Any feature ideas,
-        constructive criticism, or bugs are welcomed in the{' '}
-        <Link href="https://github.com/stephanboersma/react-bratus/issues/new?assignees=&labels=feedback&template=feedback.md&title=%5BFeedback%5D">
-          GitHub repository.
-        </Link>
+      <Title>Changelog</Title>
+      <Paragraph
+        ellipsis={
+          ellipsis ? { rows: 2, expandable: true, symbol: 'more' } : false
+        }
+      >
+        - <b>2.0.3</b> <br />
+        - Added posibility to set options in `.bratusrc.json` file
+        <br />
+        - Handle multiple components - Highlight components feature <br />
+        - Lock highlighted component and move component including descendants at
+        the same time <br />
+        - CLI command -c --compile has been changed to -p --parse <br />-{' '}
+        <b>2.0.2</b> <br />- First release
       </Paragraph>
+
       <Divider />
       <Title level={3}>Legend</Title>
       <Wrapper>
         <ReactFlow
           elements={getLayoutedElements(ELEMENTS)}
           nodeTypes={{ reactComponent: ComponentNode }}
-          paneMoveable={false}
-          zoomOnScroll={false}
+          onNodeMouseEnter={(_e, node) => highlightComponent(node, false)}
+          onNodeMouseLeave={resetHighlight}
+          onElementClick={(_e, node) => highlightComponent(node, true)}
+          onPaneClick={() =>
+            setHighlightedComponent({ componentName: null, locked: false })
+          }
         />
       </Wrapper>
     </Drawer>
@@ -142,6 +185,18 @@ const ELEMENTS = [
       linesOfCode: 240,
       outDegree: 0,
       inDegree: 1,
+    },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: 'node-7',
+    type: 'reactComponent',
+    data: {
+      label: 'Hover me to highlight',
+      component: { timesUsed: 1 },
+      linesOfCode: 10,
+      outDegree: 0,
+      inDegree: 0,
     },
     position: { x: 0, y: 0 },
   },
