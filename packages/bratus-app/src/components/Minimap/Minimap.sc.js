04f6491617c680a70655da1467ee93e37d5d2898
evgenios95 <evgeniospam@gmail.com>
2022-04-12 17:22:23 +0200

    Minimap layout

--- packages/bratus-app/src/components/Minimap/Minimap.sc.js
+++ packages/bratus-app/src/components/Minimap/Minimap.sc.js
@@ -5,11 +5,13 @@ import { baseUnit } from '../../utils/tokens/units';
 
 const StyledMiniMap = styled(MiniMap)`
   position: absolute;
-  min-width: 300px;
-  min-height: 300px;
+  width: 300px;
+  height: 220px;
   right: ${baseUnit}px;
   top: ${baseUnit}px;
-  opacity: 0.7;
+  border-radius: 15px;
+  border: 1px solid #001529;
+  opacity: 1;
 `;
 
 export default StyledMiniMap;
