b8e6d746badad5db3122f0653763bf4496ee4dcc
evgenios95 <evgeniospam@gmail.com>
2022-04-14 12:43:50 +0200

    Deleting bin and adding it to gitignore till next release

--- packages/bratus-cli/bin/parser/Builder/Attribute.d.ts
+++
@@ -1,8 +0,0 @@
-import ParsableElement from './ParsableElement';
-declare class Attribute extends ParsableElement {
-    private value;
-    constructor();
-    setValue(value: string): void;
-    getValue(): string;
-}
-export default Attribute;
