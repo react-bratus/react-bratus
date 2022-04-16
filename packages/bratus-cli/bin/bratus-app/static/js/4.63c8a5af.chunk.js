e3b8e5c97ccea4a26bf8b07d01af1357fe5f520d
evgenios95 <evgeniospam@gmail.com>
2022-04-16 13:32:35 +0200

    Removing bin

--- packages/bratus-cli/bin/bratus-app/static/js/4.63c8a5af.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@react-bratus/app"]=this["webpackJsonp@react-bratus/app"]||[]).push([[4],{922:function(p,a){p.exports={messages:{page:"page"}}}}]);
-//# sourceMappingURL=4.63c8a5af.chunk.js.map
