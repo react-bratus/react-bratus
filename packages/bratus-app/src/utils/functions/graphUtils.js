e176139c6948e48737a9b419e44867aad61cbac2
evgenios95 <evgeniospam@gmail.com>
2022-04-18 12:57:34 +0200

    App and graphutils

--- packages/bratus-app/src/utils/functions/graphUtils.js
+++ packages/bratus-app/src/utils/functions/graphUtils.js
@@ -5,18 +5,19 @@ import { baseNodeHeight, nodeWidth } from '../constants/units';
 
 var aditionalSpaceMultiplier = 2;
 
+// Initialize graph.
 const dagreGraph = new dagre.graphlib.Graph();
 
+// Default to assigning a new object as a label for each new edge.
 dagreGraph.setDefaultEdgeLabel(() => ({}));
 
 /**
+ * SOS: This function is complicated, check dagre documentation and dagre examples to get more information about it.
  * @param {*} nodesAndEdges the nodes and edges of the tree.
  * @param {*} treeLayoutDirection The layout direction of the tree. Read more here: https://g6.antv.vision/en/docs/api/graphLayout/dagre. It is set to a value, as this is the way JS understands optional params.
- * @param {*} setTreeLayoutDirection Setting the direction state, to use it for handles positioning
- * @function setDefaultEdgeLabel Default to assigning a new object as a label for each new edge.
- * @function setGraph Sets an object for the graph label
- * @function el.position Stephan: Unfortunately we need this little hack to pass a slighltiy different position to notify react flow about the change. More over we are shifting the dagre node position (anchor=center center) to the top left so it matches the react flow node anchor point (top left).
- * @returns the node elements.
+ * @param {*} setTreeLayoutDirection Setting the direction state, to use it for handles positioning.
+ * @function graphElement.position Stephan: Unfortunately we need this little hack to pass a slighltiy different position to notify react flow about the change. More over we are shifting the dagre node position (anchor=center center) to the top left so it matches the react flow node anchor point (top left).
+ * @returns layouted nodes and edges.
  */
 export const getLayoutedGraphElements = (
   nodesAndEdges,
@@ -24,9 +25,10 @@ export const getLayoutedGraphElements = (
   setTreeLayoutDirection = () => {},
   componentBackground
 ) => {
+  // Set objects for graph labels and the graph's layout direction.
   dagreGraph.setGraph({ rankdir: treeLayoutDirection });
 
-  const isHorizontalLayout = treeLayoutDirection === GraphLabels.leftToRight;
+  const isVerticalLayout = treeLayoutDirection === GraphLabels.leftToRight;
 
   nodesAndEdges.forEach((graphElement) => {
     if (isNode(graphElement)) {
@@ -67,14 +69,16 @@ export const getLayoutedGraphElements = (
 
   dagre.layout(dagreGraph);
 
+  // Inform the application about the tree layout at all times.
   setTreeLayoutDirection(treeLayoutDirection);
 
   return nodesAndEdges.map((graphElement) => {
     if (isNode(graphElement)) {
       const nodeWithPosition = dagreGraph.node(graphElement.id);
 
-      graphElement.targetPosition = isHorizontalLayout ? 'left' : 'top';
-      graphElement.sourcePosition = isHorizontalLayout ? 'right' : 'bottom';
+      // Position edge-node handle elements.
+      graphElement.targetPosition = isVerticalLayout ? 'left' : 'top';
+      graphElement.sourcePosition = isVerticalLayout ? 'right' : 'bottom';
 
       if (treeLayoutDirection === GraphLabels.topToBottom) {
         if (componentBackground.mode === BackgroundLabels.size) {
