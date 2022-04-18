03cce5a9cb991f39987eb560792d04c1cd16e90b
evgenios95 <evgeniospam@gmail.com>
2022-04-18 09:49:47 +0200

    Removing bin after npm release

--- packages/bratus-cli/bin/bratus-app/static/js/3.af96e2b8.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonp@react-bratus/app"]=this["webpackJsonp@react-bratus/app"]||[]).push([[3],{921:function(p,a){p.exports={messages:{page:"page"}}}}]);
-//# sourceMappingURL=3.af96e2b8.chunk.js.map
