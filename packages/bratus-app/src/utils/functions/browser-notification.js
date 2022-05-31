e06c92f589a2193b596da3a71768a2ac726cb36d
evgenios95 <evgeniospam@gmail.com>
2022-05-31 11:15:34 +0200

    Done

--- packages/bratus-app/src/utils/functions/browser-notification.js
+++ packages/bratus-app/src/utils/functions/browser-notification.js
@@ -1,22 +1,39 @@
 import React from 'react';
 import { Button, notification } from 'antd';
 
-export const triggerBrowserWarning = () => {
+export const triggerBrowserWarning = (
+  setIsBrowserWarningHidden,
+  isBrowserWarningHidden
+) => {
   const key = `open${Date.now()}`;
   const btn = (
-    <Button type="primary" size="small" onClick={() => notification.close(key)}>
-      I understand
+    <Button
+      type="primary"
+      size="small"
+      onClick={() => {
+        notification.close(key);
+        setIsBrowserWarningHidden(true);
+      }}
+    >
+      I understand, dont show this again
     </Button>
   );
   const title = <b>Browser Warning</b>;
 
-  notification['warning']({
-    message: title,
-    description:
-      'Dear Users, for your best experience, we recommend you using Chrome.',
-    btn,
-    key,
-    duration: 10,
-    onClose: close,
-  });
+  // No reason to display warning if we are already using Chrome
+  const isChromeAgent = navigator.userAgent.indexOf('Chrome') > -1;
+
+  const notificationTriggerCondition =
+    !isChromeAgent && !isBrowserWarningHidden;
+
+  notificationTriggerCondition &&
+    notification['warning']({
+      message: title,
+      description:
+        'Dear Users, for your best experience, we recommend you using Chrome.',
+      btn,
+      key,
+      duration: 10,
+      onClose: close,
+    });
 };
